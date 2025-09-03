3dc7d23003c14888f686f62470449995ec5e2d03
Piyush Rajput <118372177+piyushh2304@users.noreply.github.com>
2025-09-04 02:40:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
